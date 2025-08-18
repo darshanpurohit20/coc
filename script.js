68c3eb8eab897f141a8626a9ca3b9e0ff4bf6e69
Darshan Purohit <145535601+darshanpurohit07@users.noreply.github.com>
2025-08-18 16:50:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,14 +55,10 @@ fetchBtn.addEventListener("click", async () => {
       <b>Join</b>
     </a>
   </p>
-  <p><strong>Type:</strong> ${clan.type || "N/A"}</p>
-  <p><strong>Members:</strong> ${clan.members || "N/A"}</p>
-  <p><strong>Level:</strong> ${clan.clanLevel || "N/A"}</p>
-  <p><strong>Required Trophies:</strong> ${clan.requiredTrophies || "N/A"}</p>
-  <p><strong>Capital League:</strong> ${clan.capitalLeague?.name || "N/A"}</p>
-  <p><strong>Clan War League:</strong> ${clan.warLeague?.name || "N/A"}</p>
-  <img src="${clan.badgeUrls?.medium || ''}" alt="Clan Badge" style="width:50px;height:50px;"/>
+  <p><strong>Members:</strong> ${clan.members}</p>
+  <p><strong>Level:</strong> ${clan.level || "N/A"}</p>
 `;
+
 
         output.appendChild(card);
       });

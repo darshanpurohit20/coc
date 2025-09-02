d3e10d754d701c83fa33f5b4db32b073434834d5
Darshan Purohit <145535601+darshanpurohit20@users.noreply.github.com>
2025-09-02 19:14:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const clanTypeFilter = document.getElementById("clanTypeFilter");
 const minMembersFilter = document.getElementById("minMembersFilter");
 const raidLeagueFilter = document.getElementById("raidLeagueFilter");
 
-const BACKEND_URL = "https://48c6e4aadda5.ngrok-free.app/clans"; 
+const BACKEND_URL = "https://921592f4b281.ngrok-free.app/clan"; 
 const BATCH_SIZE = 20; // smaller batch to avoid 429
 let stopRequested = false; // flag to stop fetching
 
